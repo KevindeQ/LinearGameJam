fdbecc487315cff28a7c4b390ca57288fa8ff1a8
edward010 <edward_da_indo@hotmail.com>
2015-12-12 14:29:01 +0100

    Deleted unnecessary codes

--- Assets/Scripts/MatrixMultiplication.cs
+++ Assets/Scripts/MatrixMultiplication.cs
@@ -3,20 +3,6 @@ using System.Collections;
 
 public class MatrixMultiplication : MonoBehaviour {
 
-	int[, ] matrix = new int[, ]
-	{
-		{ 8, 9, 8, 2},
-		{ 3, 5, 6, 1},
-		{ 2, 3, 5, 3},
-	};
-	int[, ] matrix2 = new int[, ]
-	{
-		{ 8, 3, 1},
-		{ 1, 4, 8},
-		{ 3, 4, 1},
-		{ 4, 2, 3},
-	};
-
 	public int [,] MatrixMult(int[,] matrix1, int[,] matrix2) {
 
 		int rowMatrix1 = matrix1.GetLength(0);
@@ -34,32 +20,5 @@ public class MatrixMultiplication : MonoBehaviour {
 			}
 		}
 		return result;
-	}
-
-	void ShowMatrix(int[,] matrix) {
-		
-		int rowMatrix = matrix.GetLength(0);
-		int columnMatrix = matrix.GetLength(1);
-
-		for (int n = 0; n < columnMatrix; n++) {
-			string temp = "";
-			for (int m = 0; m < rowMatrix; m++) {
-				temp = temp + " " + matrix[n,m];
-			}
-			Debug.Log (temp);
-		}
-	}
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown ("f")) {
-			int [,] temp = new int[99,99];
-			temp = MatrixMult(matrix, matrix2);
-			ShowMatrix(temp);
-		}
 	}
 }
