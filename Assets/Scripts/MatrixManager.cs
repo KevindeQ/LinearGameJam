ccc22bcdb5b7f02864e1259cf1f25871060fece4
pardeshirzadeh <pedy_ard@hotmail.com>
2015-12-12 16:34:15 +0100

    Initial player movement

--- Assets/Scripts/MatrixManager.cs
+++ Assets/Scripts/MatrixManager.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class MatrixManager : MonoBehaviour
 {
 	public Transform block;
+    public Vector2 startTower;
+
+    public PlayerManager playerManager;
 
 	int[, ] matrix;
 
@@ -11,34 +14,74 @@ public class MatrixManager : MonoBehaviour
 	void Start () {
 		matrix = new int[, ]
         {
-			{ 2, 2, 4, 3 },
-			{ 2, 2, 3, 1 },
-            { 1, 1, 5, 3 },
+			{ 1, 2, 3, 4 },
+			{ 2, 3, 4, 5 },
+            { 3, 4, 5, 7 },
         };
         
         int rows = matrix.GetLength(0);
         int columns = matrix.GetLength(1);
-        
-        for (int x = 0; x < columns; x++)
+
+        Vector3 startPosition = Vector3.zero;
+
+        for (int col = 0; col < columns; col++)
         {
-            for (int z = 0; z < rows; z++)
+            for (int row = 0; row < rows; row++)
             {
                 GameObject tower = new GameObject();
-                tower.transform.position = new Vector3(x - columns / 2.0f, 0, -1 * (z - rows / 2.0f));
-                tower.name = string.Format("Tower ({0}, {1})", x, z);
+                tower.transform.position = new Vector3(col - columns / 2.0f, 0, -1 * (row - rows / 2.0f));
+                tower.name = string.Format("Tower ({0}, {1})", col, row);
                 tower.transform.parent = transform;
 
-                for (int i = 0; i < matrix[z, x]; i++)
+                for (int i = 0; i < matrix[row, col]; i++)
                 {
                     Transform blockInst = (Transform)Instantiate(block);
                     blockInst.name = string.Format("Block {0}", i + 1);
                     blockInst.parent = tower.transform;
                     blockInst.localPosition = new Vector3(0, i, 0);
+
+                    if (row == startTower.x && col == startTower.y)
+                    {
+                        startPosition = blockInst.position;
+                    }
                 }
             }
 		}
+
+        playerManager.transform.position = startPosition;
+        playerManager.Location = startTower;
 	}
 
-	// Update is called once per frame
-	void Update () {}
+	public bool CanReach (Direction dir)
+    {
+        int locationX = (int)playerManager.Location.x;
+        int locationY = (int)playerManager.Location.y;
+
+        switch (dir) {
+            case Direction.North:
+                if (locationY - 1 == -1) return false;
+                return Mathf.Abs(matrix[locationY, locationX] - matrix[locationY - 1, locationX]) <= 1;
+            case Direction.South:
+                if (locationY + 1 == matrix.GetLength(0)) return false;
+                return Mathf.Abs(matrix[locationY, locationX] - matrix[locationY + 1, locationX]) <= 1;
+            case Direction.West:
+                if (locationX - 1 == -1) return false;
+                return Mathf.Abs(matrix[locationY, locationX] - matrix[locationY, locationX - 1]) <= 1;
+            case Direction.East:
+                if (locationX + 1 == matrix.GetLength(1)) return false;
+                return Mathf.Abs(matrix[locationY, locationX] - matrix[locationY, locationX + 1]) <= 1;
+            default:
+                return false;
+        }
+    }
+
+    public int GetHeight(Vector2 location)
+    {
+        return GetHeight((int)location.x, (int)location.y);
+    }
+
+    public int GetHeight (int x, int y)
+    {
+        return matrix[y, x];
+    }
 }
