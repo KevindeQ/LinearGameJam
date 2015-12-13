6ebfb6808e6fde57ff2be2f27e48291ac3e07e83
KevindeQ <k.dequillettes@gmail.com>
2015-12-13 14:16:19 +0100

    Basic bosslevel.

---
+++ Assets/Scripts/TurretAimController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretAimController : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject Bullet;
+    Vector3 BulletSpawnLocation;
+
+    void Start()
+    {
+        BulletSpawnLocation = transform.position + new Vector3(0.6f, 1.27f, 0.0f);
+        Instantiate(Bullet, BulletSpawnLocation, Quaternion.identity);
+    }
+
+	void Update ()
+    {
+	    
+	}
+}
